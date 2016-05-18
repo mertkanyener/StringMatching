3c715a11472e53cd36696acd151d7cd2ecfc9aee
Mertkan <mertkan.yener@bilgiedu.net>
2016-05-18 13:19:16 +0300

    Produce alphabet method created and tested.

--- StringMatching/src/StrMatcher.java
+++ StringMatching/src/StrMatcher.java
@@ -1,4 +1,16 @@
+import java.util.Vector;
 
 public class StrMatcher {
+	
+	public Vector<Character> produceAlph(String s){
+		Vector<Character> result = new Vector<Character>();
+		for(int i = 0; i < s.length(); i++){
+			char x = s.charAt(i);
+			if(!result.contains(x)){
+				result.add(x);
+			}
+		}
+		return result;
+	}
 
 }
--- StringMatching/src/Test.java
+++ StringMatching/src/Test.java
@@ -1,4 +1,9 @@
 
 public class Test {
+	public static void main(String[] args){
+		String p = "aabcda";
+		StrMatcher s = new StrMatcher();
+		System.out.println(s.produceAlph(p));
+	}
 
 }
